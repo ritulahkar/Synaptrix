3119165f46d3029d2b4b51516d5927f292232ce5
Ritu Lahkar <lahkarritu@gmail.com>
2025-08-08 21:49:04 +0530

    Revert "WIP: saving changes"
    This reverts commit 2ccb823abe4c627468260fee76bd8bac500a30cf.

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=X11");
-    println!("cargo:rustc-link-lib=Xext");       // Required by some X11 functions
-    println!("cargo:rustc-link-lib=Xrender");    // Sometimes needed for rendering support
-}
